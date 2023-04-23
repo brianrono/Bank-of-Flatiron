6b38acfb0f2286b77c7c1d4482c130b5595d19d3
brianrono <brianrono16@gmail.com>
2023-04-23 23:20:04 +0300

    Fixed bugs denying web display

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -4,16 +4,18 @@ import TransactionForm from './Components/transactionForm';
 import TransactionList from './Components/transactionList';
 import './App.css';
 
-const App = () => {
+      const App = () => {
       const [transactions, setTransactions] = useState([]);
+
       useEffect(() => {
-        // Fetch transactions from the backend server
-            const fetchTransactions = async () => {
-                  try {
-                        const response = ('http://localhost:3000/transactions'); // maxios library makes the API call to the server
-                        setTransactions(response.data);
-                  } catch (error) {
-                  console.error('Error fetching transactions:', error);
+      // Fetch transactions from the backend server
+      const fetchTransactions = async () => {
+            try {
+            const response = await fetch('http://localhost:3000/transactions');
+            const data = await response.json();
+            setTransactions(data);
+            } catch (error) {
+            console.error('Error fetching transactions:', error);
             }
       };
 
@@ -22,31 +24,35 @@ const App = () => {
 
       // Event handler for adding a new transaction
       const handleAddTransaction = (newTransaction) => {
-            // Generate a unique ID for the new transaction
-            newTransaction.id = Math.floor(Math.random() * 1000000);
-            // Update the state with the new transaction
-            setTransactions([...transactions, newTransaction]);
+      // Generate a unique ID for the new transaction
+      newTransaction.id = Math.floor(Math.random() * 1000000);
+      // Update the state with the new transaction
+      setTransactions([...transactions, newTransaction]);
       };
 
       // Event handler for searching transactions
       const handleSearch = (searchTerm) => {
-            // Filter transactions based on search term
-            const filteredTransactions = transactions.filter(transaction => transaction.description.toLowerCase().includes(searchTerm.toLowerCase()));
-            // Update the state with filtered transactions
-            setTransactions(filteredTransactions);
-      };    
+      // Filter transactions based on search term
+      const filteredTransactions = transactions.filter(
+            (transaction) =>
+            transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      // Update the state with filtered transactions
+      setTransactions(filteredTransactions);
+      };
 
       return (
-            <div className='App'>
+      <div className='App'>
             <h1>Bank Transactions</h1>
             <SearchBar onSearch={handleSearch} />
             <TransactionList transactions={transactions} />
             <TransactionForm onAddTransaction={handleAddTransaction} />
-            </div>
+      </div>
       );
-};
+      };
 
-export default App;
+      export default App;
+
 
 
 // class App extends Component {
